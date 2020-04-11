4c85995cca9d326b52f652e148ec33cc69538bd6
Karl Loomes <kloomes@hotmail.com>
2020-04-11 18:03:33 +0100

    Routes added, will need changing as go along

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :stats, except: :show
+  resources :sessions
+  resources :cardios
+  resources :exercises do
+    resources :setsets, only: [ :index, :new, :create ]
+  end
+  resources :setsets, only: [ :edit, :update ]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,72 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_161538) do
+ActiveRecord::Schema.define(version: 2020_04_11_164334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cardios", force: :cascade do |t|
+    t.string "exercise"
+    t.string "workout"
+    t.integer "hours"
+    t.integer "minutes"
+    t.integer "seconds"
+    t.float "distance"
+    t.float "pace"
+    t.float "speed"
+    t.integer "avg_hr"
+    t.integer "max_hr"
+    t.integer "kcal"
+    t.text "notes"
+    t.bigint "session_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["session_id"], name: "index_cardios_on_session_id"
+  end
+
+  create_table "exercises", force: :cascade do |t|
+    t.string "name"
+    t.text "notes"
+    t.bigint "session_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["session_id"], name: "index_exercises_on_session_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.date "date"
+    t.time "time"
+    t.integer "hours"
+    t.integer "minutes"
+    t.integer "seconds"
+    t.string "type"
+    t.text "notes"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "setsets", force: :cascade do |t|
+    t.float "weight"
+    t.integer "reps"
+    t.bigint "exercise_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["exercise_id"], name: "index_setsets_on_exercise_id"
+  end
+
+  create_table "stats", force: :cascade do |t|
+    t.float "weight"
+    t.float "bf"
+    t.integer "bmi"
+    t.float "lean"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_stats_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +89,9 @@ ActiveRecord::Schema.define(version: 2020_04_11_161538) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cardios", "sessions"
+  add_foreign_key "exercises", "sessions"
+  add_foreign_key "sessions", "users"
+  add_foreign_key "setsets", "exercises"
+  add_foreign_key "stats", "users"
 end

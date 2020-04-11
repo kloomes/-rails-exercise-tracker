3cce5a4842a2e6adfac793d3cbbb34e34098900b
Karl Loomes <kloomes@hotmail.com>
2020-04-11 18:07:52 +0100

    Controllers added

---
+++ app/controllers/setsets_controller.rb
@@ -0,0 +1,2 @@
+class SetsetsController < ApplicationController
+end
---
+++ test/controllers/setsets_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SetsetsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end

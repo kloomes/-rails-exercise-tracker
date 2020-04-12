172279f253bbd4d1865e6c397ee44358cea00348
Karl Loomes <kloomes@hotmail.com>
2020-04-12 15:30:46 +0100

    New session page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'sessions#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [ :edit, :update, :show ]
+  resources :users, only: [ :edit, :update, :show ] do
+    resources :sessions, only: [ :index, :new, :create ]
+  end
   resources :stats, except: :show
-  resources :sessions
+  resources :sessions, only: [ :show, :edit, :destroy ]
   resources :cardios
   resources :exercises do
     resources :setsets, only: [ :index, :new, :create ]
